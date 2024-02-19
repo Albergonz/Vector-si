fed36416731ea7f71fae806c8b950800d4b73981
Acer <Acer@LAPTOP-9SISV8A7>
2024-02-19 19:27:36 +0100

    Aggiungere i file di progetto.

---
+++ Vector si/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_si
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(6, 3);
+            Vector v2 = new Vector(7, 2);
+            Console.WriteLine("{0}", v1 + v2);
+            Console.ReadLine();
+        }
+    }
+}
